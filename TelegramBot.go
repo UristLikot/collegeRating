3e2543272af1c657b6adce9a28f115f8e7ea46ea
UristLikot <chipkazakov@gmail.com>
2023-08-01 19:43:12 +0300

    url fix ??

--- TelegramBot.go
+++ TelegramBot.go
@@ -23,8 +23,8 @@ func telegramBot() {
 			continue
 		}
 		nntc := NntcHandler()
-		logistics := LogisticsGetOriginals("https://shorturl.at/brQZ3", "Логистика речное")
-		cs := LogisticsGetOriginals("https://shorturl.at/GKM78", "Информатика речное")
+		logistics := LogisticsGetOriginals("https://vsuwt.ru/files/abitur/lsitOfAbiturs/spo/23.02.01%20%D0%9E%D1%80%D0%B3%D0%B0%D0%BD%D0%B8%D0%B7%D0%B0%D1%86%D0%B8%D1%8F%20%D0%BF%D0%B5%D1%80%D0%B5%D0%B2%D0%BE%D0%B7%D0%BE%D0%BA%20%D0%B8%20%D1%83%D0%BF%D1%80%D0%B0%D0%B2%D0%BB%D0%B5%D0%BD%D0%B8%D0%B5%20%D0%BD%D0%B0%20%D1%82%D1%80%D0%B0%D0%BD%D1%81%D0%BF%D0%BE%D1%80%D1%82%D0%B5%20(%D0%BF%D0%BE%20%D0%B2%D0%B8%D0%B4%D0%B0%D0%BC)%209%20%D0%BA%D0%BB%D0%B0%D1%81%D1%81%D0%BE%D0%B2%20-%20%D0%9E%D1%87%D0%BD%D0%B0%D1%8F%20-%20%D0%B1%D1%8E%D0%B4%D0%B6%D0%B5%D1%82_%D0%91.html", "Логистика речное")
+		cs := LogisticsGetOriginals("https://vsuwt.ru/files/abitur/lsitOfAbiturs/spo/09.02.07%20%D0%98%D0%BD%D1%84%D0%BE%D1%80%D0%BC%D0%B0%D1%86%D0%B8%D0%BE%D0%BD%D0%BD%D1%8B%D0%B5%20%D1%81%D0%B8%D1%81%D1%82%D0%B5%D0%BC%D1%8B%20%D0%B8%20%D0%BF%D1%80%D0%BE%D0%B3%D1%80%D0%B0%D0%BC%D0%BC%D0%B8%D1%80%D0%BE%D0%B2%D0%B0%D0%BD%D0%B8%D0%B5%209%20%D0%BA%D0%BB%D0%B0%D1%81%D1%81%D0%BE%D0%B2%20-%20%D0%9E%D1%87%D0%BD%D0%B0%D1%8F%20-%20%D0%B1%D1%8E%D0%B4%D0%B6%D0%B5%D1%82_%D0%91.html", "Информатика речное")
 		msg := tgbotapi.NewMessage(update.Message.Chat.ID, fmt.Sprintf("%v\n%v\n%v", nntc, logistics, cs))
 
 		bot.Send(msg)
