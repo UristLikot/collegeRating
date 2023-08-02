9bec1e4b1117192e86aeb84074b6fb75fefd2512
UristLikot <chipkazakov@gmail.com>
2023-08-02 07:58:04 +0300

    expose fix???

--- main.go
+++ main.go
@@ -1,6 +1,12 @@
 package main
 
-func main() {
+import "net/http"
 
+func main() {
 	telegramBot()
+	http.HandleFunc("/", handler)
+	http.ListenAndServe(":80", nil)
+}
+func handler(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("200"))
 }
